2dfc1769ebbc1bec256d772e3ab7d13e8d58c472
aaronmlee <aaron@blackbirdit.com>
2014-05-02 23:10:33 -0700

    everything working

--- consul-monitor/consul_start.sh
+++ consul-monitor/consul_start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-exec /usr/bin/consul agent -config-dir=/etc/consul.d -data-dir=/tmp/consul -log-level debug -join $CONSUL_LOCAL_PORT_8300_TCP_ADDR
+#client=`ifconfig | grep 'inet addr:'| grep  '172' | cut -d: -f2 | awk '{ print $1}'`
+
+exec /usr/bin/consul agent -config-dir=/etc/consul.d -data-dir=/tmp/consul -log-level debug -join $CONSUL_LOCAL_PORT_8300_TCP_ADDR -client 0.0.0.0 --ui-dir /consul-ui
